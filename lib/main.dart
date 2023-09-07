21fa8f8a72331a418597fb6b18084c3a4c5a7145
KhalidPC <khalid.ahmad063@gmail.com>
2023-09-07 09:38:27 +0600

    env setup is done

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Ecommerce of Ostad, Batch - 3',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return HomeScreenUI();
+  }
+
+}
+
+class HomeScreenUI extends State<HomeScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ecommerce App of Ostad'),
+      ),
+    );
+  }
+}
